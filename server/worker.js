33cdf557c877db4d104c35e15bfdb9b92c595266
Shams Ali <ali.s90@outlook.com>
2016-07-07 14:17:44 -0700

    twilio is using get for url queue

--- server/worker.js
+++ server/worker.js
@@ -1,23 +1,25 @@
-var Consumer = require('sqs-consumer');
-var AWS = require('aws-sdk');
-var twilio = require('./twilio.js');
 require('dotenv').config({silent: true});
-AWS.config.update({
-  region: process.env.AWS_REGION
-});
+var AWS = require('aws-sdk');
+AWS.config.update({ region: process.env.AWS_REGION });
+var Consumer = require('sqs-consumer');
+var twilio = require('./twilio.js');
 
-var app = Consumer.create({
-  queueUrl: process.env.QUEUE_URL'https://sqs.us-east-1.amazonaws.com/971458161724/twilio-channel',
-  handleMessage: function(data, done){
-    twilio(data);
-    done();
-  },
-  sqs: new AWS.SQS()
-});
-app.on('error', function(err){
-  console.log(err.message);
-  return err.message
+var sqs = new AWS.SQS();
+
+sqs.getQueueUrl({ QueueName: 'twilio-channel' }, function(err, data) {
+  if (err) return console.log(err);
+  var app = Consumer.create({
+    queueUrl: data.QueueUrl,
+    handleMessage: function(data, done){
+      twilio(data);
+      done();
+    },
+    sqs: new AWS.SQS()
+  });
+  app.on('error', function(err){
+    console.log(err.message);
+    return err.message
+  })
+  app.start();
+  module.exports = app;
 })
-app.start();
-
-module.exports = app;
