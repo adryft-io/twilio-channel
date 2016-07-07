044b2ab61637b830ad4a4fb815258197c61d3f0b
Shams Ali <ali.s90@outlook.com>
2016-07-07 14:29:39 -0700

    refactored

--- server/worker.js
+++ server/worker.js
@@ -1,22 +1,22 @@
 require('dotenv').config({silent: true});
-var AWS = require('aws-sdk');
+var AWS = require('aws-sdk')
 AWS.config.update({ region: process.env.AWS_REGION });
 var Consumer = require('sqs-consumer');
 var twilio = require('./twilio.js');
 
 var sqs = new AWS.SQS();
 
-sqs.getQueueUrl({ QueueName: 'twilio-channel' }, function(err, data) {
+sqs.getQueueUrl({ QueueName: 'twilio-channel' }, (err, data) => {
   if (err) return console.log(err);
   var app = Consumer.create({
     queueUrl: data.QueueUrl,
-    handleMessage: function(data, done){
+    handleMessage: (data, done) => {
       twilio(data);
       done();
     },
-    sqs: new AWS.SQS()
+    sqs: sqs
   });
-  app.on('error', function(err){
+  app.on('error', (err) => {
     console.log(err.message);
     return err.message
   })
